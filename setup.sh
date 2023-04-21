92fbd7f4265f42084d4a77f66241139a6df8fe60
Carl Hvarfner <carl.hvarfner@live.se>
2023-04-21 09:07:54 +0200

    submod

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 conda activate jes-botorch
-export PYTHONPATH="${PWD}/LassoBench:${PWD}/gpytorch:${PWD}/Ax:${PWD}/PiJES:${PYTHONPATH}"
+export PYTHONPATH="${PWD}/camb:${PWD}/LassoBench:${PWD}/gpytorch:${PWD}/Ax:${PWD}/PiJES:${PYTHONPATH}"
 
